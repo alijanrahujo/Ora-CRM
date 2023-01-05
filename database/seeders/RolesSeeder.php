7c08e147c906d9b3a1d76b4ad0942a25cd82a45f
alijanrahujo <68347990+alijanrahujo@users.noreply.github.com>
2023-01-05 11:52:27 +0500

    Initial commit

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $roles = [
+            'Admin',
+            'User',
+            'Client'
+        ];
+
+        foreach($roles as $role)
+        {
+            Role::create(['name' => $role]);
+        }
+    }
+}
